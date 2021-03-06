0af1fb305ba357f242d398548c0ad5de9a0565a4
abzen <abovezenith@gmail.com>
2021-03-06 20:52:04 +0000

    add path aliases

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,9 @@
 import { DarkModeSwitch } from '../components/DarkModeSwitch';
 import dynamic from 'next/dynamic';
-import Page from '../components/common/page';
+import Page from 'components/common/page';
 
-const RMRKInit = dynamic(() => import('../components/rmrk-init'), { ssr: false });
-const NFTList = dynamic(() => import('../components/nft-list/list'), { ssr: false });
+const RMRKInit = dynamic(() => import('components/rmrk-init'), { ssr: false });
+const NFTList = dynamic(() => import('components/nft-list/list'), { ssr: false });
 
 const Index = () => {
   const title = 'Home';
--- src/pages/mint.tsx
+++ src/pages/mint.tsx
@@ -1,9 +1,9 @@
 import { Text } from '@chakra-ui/react';
-import { DarkModeSwitch } from '../components/DarkModeSwitch';
-import Page from '../components/common/page';
+import { DarkModeSwitch } from 'components/DarkModeSwitch';
+import Page from 'components/common/page';
 import dynamic from 'next/dynamic';
 
-const RMRKInit = dynamic(() => import('../components/rmrk-init'), { ssr: false });
+const RMRKInit = dynamic(() => import('components/rmrk-init'), { ssr: false });
 
 const Mint = () => {
   const title = 'Mint';
