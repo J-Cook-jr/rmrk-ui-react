ac68ceed76cdfdae60bbc2acb98c137dfe07199f
abzen <abovezenith@gmail.com>
2021-03-13 22:52:22 +0000

    add path map to nextjs config

--- next.config.js
+++ next.config.js
@@ -1,14 +1,20 @@
 module.exports = {
-    webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
-        // Note: we provide webpack above so you should not `require` it
-        // Perform customizations to webpack config
-        config.module.rules.push({
-            test: /\.mjs$/,
-            include: /node_modules/,
-            type: "javascript/auto"
-        });
+  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+    // Note: we provide webpack above so you should not `require` it
+    // Perform customizations to webpack config
+    config.module.rules.push({
+      test: /\.mjs$/,
+      include: /node_modules/,
+      type: 'javascript/auto',
+    });
 
-        // Important: return the modified config
-        return config
-    },
-}
+    // Important: return the modified config
+    return config;
+  },
+  exportPathMap: async function (defaultPathMap, { dev, dir, outDir, distDir, buildId }) {
+    return {
+      '/': { page: '/' },
+      '/mint': { page: '/mint' },
+    };
+  },
+};
