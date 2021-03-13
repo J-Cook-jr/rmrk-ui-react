972bbd8322a3e602bc47c52d238578edc117d71f
abzen <abovezenith@gmail.com>
2021-03-13 21:51:29 +0000

    minting page layout update

--- src/components/mint/mint-page.tsx
+++ src/components/mint/mint-page.tsx
@@ -1,9 +1,19 @@
 import React from 'react';
-import { Box, Text } from '@chakra-ui/react';
+import { Box, SimpleGrid } from '@chakra-ui/react';
+import MintCollectionForm from 'components/mint/forms/mint-collection-form';
+import MintNftForm from 'components/mint/forms/mint-nft-form';
+import SubmitRemarkForm from 'components/mint/forms/submit-remark-form';
 
 const MintPage = () => (
   <Box data-name="mint-page">
-    <Text>This page will be a form to mind new NFT on Kusama Remark</Text>
+    <SimpleGrid mb={16} columns={[1, 1, 2]} spacing={[10, 10, 6]}>
+      <MintCollectionForm />
+      <SubmitRemarkForm />
+    </SimpleGrid>
+    <SimpleGrid mb={16} columns={[1, 1, 2]} spacing={[10, 10, 6]}>
+      <MintNftForm />
+      <SubmitRemarkForm />
+    </SimpleGrid>
   </Box>
 );
 
