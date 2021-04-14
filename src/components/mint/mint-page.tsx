e9396b00c5866480ea8329439469b147dc08a34e
abzen <abovezenith@gmail.com>
2021-04-14 22:03:23 +0100

    ui page container

--- src/components/mint/mint-page.tsx
+++ src/components/mint/mint-page.tsx
@@ -3,9 +3,10 @@ import { Box, SimpleGrid } from '@chakra-ui/react';
 import MintCollectionForm from 'components/mint/forms/mint-collection-form';
 import MintNftForm from 'components/mint/forms/mint-nft-form';
 import SubmitRemarkForm from 'components/mint/forms/submit-remark-form';
+import PageContainer from 'components/common/page-container';
 
 const MintPage = () => (
-  <Box data-name="mint-page">
+  <PageContainer data-name="mint-page">
     <SimpleGrid mb={16} columns={[1, 1, 2]} spacing={[10, 10, 6]}>
       <MintCollectionForm />
       <SubmitRemarkForm />
@@ -14,7 +15,7 @@ const MintPage = () => (
       <MintNftForm />
       <SubmitRemarkForm />
     </SimpleGrid>
-  </Box>
+  </PageContainer>
 );
 
 export default MintPage;
